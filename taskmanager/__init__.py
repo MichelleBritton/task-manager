83117b73220ef0d06d1e39a439c86b21b4587e76
Michelle Britton <michelle@daisy-webdesign.co.uk>
2023-11-07 17:35:02 +0000

    commit

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,22 +1,22 @@
 import os
+import re
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
-    import env # noqa
+    import env  # noqa
 
-# Create an instance of the imported Flask() class and store in a variable
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 if os.environ.get("DEVELOPMENT") == "True":
-     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
- else:
-     uri = os.environ.get("DATABASE_URL")
-     if uri.startswith("postgres://"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")  # local
+else:
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
-     app.config["SQLALCHEMY_DATABASE_URI"] = uri
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
 
-# Create an instance of the SQLAlchemy class which will be assigned to a variable and set to the instance of our Flask app
 db = SQLAlchemy(app)
 
 from taskmanager import routes  # noqa
